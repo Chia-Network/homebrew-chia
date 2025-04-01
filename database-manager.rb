74a3789a63ae6f4baa6401d3aba1c9e9825de129
Chia Automation <automation@chia.net>
2025-04-01 19:52:12 +0000

    Updating database-manager to version 0.2.1

--- database-manager.rb
+++ database-manager.rb
@@ -1,13 +1,13 @@
 class DatabaseManager < Formula
   desc "Manages users and databases in a database cluster based on a yaml config"
   homepage "https://github.com/Chia-Network/database-manager"
-  version "0.2.0"
+  version "0.2.1"
   license "Apache-2.0"
 
   on_macos do
     on_intel do
-      url "https://github.com/Chia-Network/database-manager/releases/download/0.2.0/database-manager-darwin-amd64.zip"
-      sha256 "9f4a5c702195052bcc9dbb18fe60919360e0dfd845240e361b69683021993709"
+      url "https://github.com/Chia-Network/database-manager/releases/download/0.2.1/database-manager-darwin-amd64.zip"
+      sha256 "90fe0d3c823fb0089735be4bb97c9cc99fda799ac5555c31913fda523b77bd93"
 
       def install
         bin.install "database-manager"
@@ -15,8 +15,8 @@ class DatabaseManager < Formula
       end
     end
     on_arm do
-      url "https://github.com/Chia-Network/database-manager/releases/download/0.2.0/database-manager-darwin-arm64.zip"
-      sha256 "553f5f4152ac9f7e0ff2bebf7f33e344a86fbc33467f522670e90fb5ed38ea03"
+      url "https://github.com/Chia-Network/database-manager/releases/download/0.2.1/database-manager-darwin-arm64.zip"
+      sha256 "38ccfa4c069a8ed7953390b19bec89abd97245c37a471e592ff736a687eab320"
 
       def install
         bin.install "database-manager"
