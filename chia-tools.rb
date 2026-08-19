0ef05e84376595a5c4b5e67ddef5bdb68b4c6509
Chia Automation <automation@chia.net>
2026-08-19 19:46:12 +0000

    Updating chia-tools to version 1.3.11

--- chia-tools.rb
+++ chia-tools.rb
@@ -1,13 +1,13 @@
 class ChiaTools < Formula
   desc "Collection of CLI tools for working with Chia Blockchain"
   homepage "https://github.com/chia-network/chia-tools"
-  version "1.3.10"
+  version "1.3.11"
   license "Apache-2.0"
 
   on_macos do
     on_intel do
-      url "https://github.com/Chia-Network/chia-tools/releases/download/1.3.10/chia-tools-darwin-amd64.zip"
-      sha256 "b5c705c11db23f91517fcee23eccfec610f7d1fc83156e78268886c3ca738669"
+      url "https://github.com/Chia-Network/chia-tools/releases/download/1.3.11/chia-tools-darwin-amd64.zip"
+      sha256 "9eab265f93daa2614b131f59ba24df6b327220e9bb250d704fbbe6df4fe1eb24"
 
       def install
         bin.install "chia-tools"
@@ -15,8 +15,8 @@ class ChiaTools < Formula
       end
     end
     on_arm do
-      url "https://github.com/Chia-Network/chia-tools/releases/download/1.3.10/chia-tools-darwin-arm64.zip"
-      sha256 "600d21ed784a4b67f3e5975639984fc04b69a41c36ab4088368f46c4a15a7f51"
+      url "https://github.com/Chia-Network/chia-tools/releases/download/1.3.11/chia-tools-darwin-arm64.zip"
+      sha256 "8de99d7f26b119d73414cceedace5fb688052dfaa1fcd4b7be7c7b4dc9e6742f"
 
       def install
         bin.install "chia-tools"
